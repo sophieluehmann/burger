5866116a6fd21a279dc274113758d8ce77b29427
Sophie OT <SophieOT@D-10-18-240-175.dhcp4.washington.edu>
2019-06-04 20:36:43 -0700

    initial files

---
+++ config/orm.js
@@ -0,0 +1,54 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, funtion(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
