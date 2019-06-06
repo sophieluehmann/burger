09b8675e37d8a4e1351ceec37df1c38827a5ff87
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-06-06 15:23:56 -0700

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,16 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    hose: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
